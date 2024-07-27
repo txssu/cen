4a6a32c922e7e8bd65f04c338e247cda2bda690c
Eugene <git@telegrafo.ru>
2024-07-27 22:10:29 +0500

    Make desktop personal settings frontend

--- lib/cen_web/live/user_settings/personal_info_live.ex
+++ lib/cen_web/live/user_settings/personal_info_live.ex
@@ -8,53 +8,56 @@ defmodule CenWeb.UserSettings.PersonalInfoLive do
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <div class="col-span-4">
+    <div class="col-span-4 lg:col-span-3">
       <Components.navigation current_page={:personal} />
     </div>
 
-    <div class="mt-[2.1875rem] gap-[1.875rem] mb-2.5 flex">
-      <div class="flex-shrink-0 flex-grow-0">
-        <div class="w-[4.375rem] h-[4.375rem] bg-accent inline-block rounded-full"></div>
+    <div class="col-span-4 lg:col-start-5">
+      <div class="mt-[2.1875rem] gap-[1.875rem] mb-2.5 flex lg:mb-[3.4375rem]">
+        <div class="flex-shrink-0 flex-grow-0">
+          <div class="w-[4.375rem] h-[4.375rem] bg-accent inline-block rounded-full lg:w-[6.75rem] lg:h-[6.75rem]">
+          </div>
+        </div>
+        <div class="flex flex-grow-0 flex-col justify-center">
+          <.button class="flex-grow-0 bg-accent-hover uppercase py-[0.9375rem] text-title-text px-5 text-nowrap">
+            <%= dgettext("users", "Удалить аккаунт") %>
+          </.button>
+        </div>
       </div>
-      <div class="flex flex-grow-0 flex-col justify-center">
-        <.button class="flex-grow-0 bg-accent-hover uppercase py-[0.9375rem] text-title-text px-5 text-nowrap">
-          <%= dgettext("users", "Удалить аккаунт") %>
-        </.button>
-      </div>
-    </div>
 
-    <div class="col-span-4">
-      <.simple_form
-        for={@personal_info_form}
-        id="personal_info_form"
-        phx-submit="update_personal_info"
-        phx-change="validate_personal_info"
-      >
-        <.input
-          field={@personal_info_form[:fullname]}
-          type="text"
-          placeholder={dgettext("users", "ФИО")}
-          required
-        />
-        <.input
-          :if={@user_role == :applicant}
-          field={@personal_info_form[:birthdate]}
-          type="date"
-          placeholder={dgettext("users", "Дата рождения")}
-          required
-        />
-        <.input
-          field={@personal_info_form[:phone_number]}
-          type="text"
-          placeholder={dgettext("users", "Номер телефона")}
-          required
-        />
-        <:actions>
-          <.arrow_button>
-            <%= dgettext("users", "Сохранить") %>
-          </.arrow_button>
-        </:actions>
-      </.simple_form>
+      <div class="col-span-4">
+        <.simple_form
+          for={@personal_info_form}
+          id="personal_info_form"
+          phx-submit="update_personal_info"
+          phx-change="validate_personal_info"
+        >
+          <.input
+            field={@personal_info_form[:fullname]}
+            type="text"
+            placeholder={dgettext("users", "ФИО")}
+            required
+          />
+          <.input
+            :if={@user_role == :applicant}
+            field={@personal_info_form[:birthdate]}
+            type="date"
+            placeholder={dgettext("users", "Дата рождения")}
+            required
+          />
+          <.input
+            field={@personal_info_form[:phone_number]}
+            type="text"
+            placeholder={dgettext("users", "Номер телефона")}
+            required
+          />
+          <:actions>
+            <.arrow_button>
+              <%= dgettext("users", "Сохранить") %>
+            </.arrow_button>
+          </:actions>
+        </.simple_form>
+      </div>
     </div>
     """
   end
