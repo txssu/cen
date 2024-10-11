2aba0630cf17b3fdf21b1fb8edee00894ec0bd71
txssu <git@telegrafo.ru>
2024-10-11 21:02:48 +0500

    Fix lint

--- lib/cen/release.ex
+++ lib/cen/release.ex
@@ -5,17 +5,19 @@ defmodule Cen.Release do
   """
   @app :cen
 
+  @spec migrate() :: term()
   def migrate do
     load_app()
 
     for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+      {:ok, _a1, _a2} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
+  @spec rollback(Ecto.Repo.t(), term()) :: term()
   def rollback(repo, version) do
     load_app()
-    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+    {:ok, _a1, _a2} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
   defp repos do
