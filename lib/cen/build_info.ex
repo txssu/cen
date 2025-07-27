48c4d5e75a7411e64a8ca709277f823f19f756e1
txssu <z.ribin20@gmail.com>
2025-07-27 14:29:15 +0500

    Update build_info.ex

--- lib/cen/build_info.ex
+++ lib/cen/build_info.ex
@@ -1,11 +1,13 @@
 defmodule Cen.BuildInfo do
   @moduledoc false
 
-  @external_resource git_revision_file = ".git/HEAD"
+  head = ".git/HEAD"
+
+  if File.exists?(head), do: @external_resource(head)
 
   git_hash_file =
-    if File.exists?(git_revision_file) do
-      head_contents = File.read!(git_revision_file)
+    if File.exists?(head) do
+      head_contents = File.read!(head)
 
       if String.starts_with?(head_contents, "ref:") do
         ref_path =
@@ -17,15 +19,16 @@ defmodule Cen.BuildInfo do
       end
     end
 
-  @external_resource git_hash_file
+  if File.exists?(git_hash_file), do: @external_resource(git_hash_file)
 
   hash =
-    git_hash_file
-    |> File.read!()
-    |> String.slice(0, 7)
+    if File.exists?(git_hash_file) do
+      git_hash_file
+      |> File.read!()
+      |> String.slice(0, 7)
+    else
+      nil
+    end
 
-  @git_short_hash hash
-
-  @spec git_short_hash() :: String.t()
-  def git_short_hash, do: @git_short_hash
+  def git_short_hash, do: unquote(hash)
 end
